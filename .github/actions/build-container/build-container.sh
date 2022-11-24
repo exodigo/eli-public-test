12655a8f06fd4d367ce731506f3f563518b24be6
Eli Gavril <eli.gavril@exodigo.ai>
2022-11-24 19:05:12 +0200

    ci experiments

--- .github/actions/build-container/build-container.sh
+++ .github/actions/build-container/build-container.sh
@@ -3,10 +3,6 @@
 OCI_ENGINE="${OCI_ENGINE:-podman}"
 OCI_CONTAINER_FILE="${OCI_CONTAINER_FILE:-Containerfile}"
 
-echo "aaaaaa"
-echo "bbbbb: ${OCI_ARG_LOCAL_TAG}"
-echo "ccccc: ${OCI_ARG_DIST}"
-
 eval "set -- "$(set | sed -n -e 's/^OCI_ARG_[^=]*=// p')""
 exec ${OCI_ENGINE} image build \
   --secret id=pip_secret,src="${HOME}/.config/pip/pip.conf" \
