51c4e5fa28d5a37bb4023d5ed4d675f48fe1f1fc
Eli Gavril <eli.gavril@exodigo.ai>
2022-11-27 19:38:23 +0200

    ci experiments

--- .github/actions/build-container/build-container.sh
+++ .github/actions/build-container/build-container.sh
@@ -5,6 +5,8 @@ OCI_CONTAINER_FILE="${OCI_CONTAINER_FILE:-Containerfile}"
 
 eval "set -- "$(set | sed -n -e 's/^OCI_ARG_[^=]*=// p')""
 exec ${OCI_ENGINE} image build \
+  --secret id=pip_secret,src="${HOME}/.config/pip/pip.conf"
+  --build-arg=pip_creds=".config/pip/pip.conf"
   --secret id=twine_secret,src="${HOME}/.pypirc" \
   --build-arg twine_creds=".pypirc" \
   --secret id=npm_secret,src="${HOME}/.npmrc" \
